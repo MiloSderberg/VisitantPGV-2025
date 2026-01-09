024e2e47704fa7f0063043f7133ec9c272358ef0
OrvarLBS <oscar.rydensjo@elev.ga.lbs.se>
2026-01-09 13:31:12 +0100

    .

--- Visitant/Assets/Code/enemyhealthbar.cs
+++ Visitant/Assets/Code/enemyhealthbar.cs
@@ -7,6 +7,13 @@ public class enemyhealthbar : MonoBehaviour
     void Start()
     {
 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("canDamageEnemy"))
